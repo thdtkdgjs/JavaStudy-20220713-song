53fbcb01c2c0e3771a09bf06512ae415bd97b170
thdtkdgjs <smhssh9@naver.com>
2022-07-15 11:06:13 +0900

    챕터 3 출력

---
+++ src/J03_연산자/Opereation3.java
@@ -0,0 +1,25 @@
+package J03_연산자;
+
+/*
+ * Ctrl + space -> 자동완성
+ */
+
+public class Opereation3 {
+
+	public static void main(String[] args) {
+		int num = 50;
+		
+		System.out.println(++num);		
+		System.out.println(num);	
+		System.out.println(num++);
+		System.out.println(num);
+		
+		System.out.println(--num);		
+		System.out.println(num);	
+		System.out.println(num--);
+		System.out.println(num);
+		
+
+	}
+
+}
