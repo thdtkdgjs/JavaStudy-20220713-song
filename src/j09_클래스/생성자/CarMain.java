01c0fc67af0d71f30db648279dddb1c5f3cae607
thdtkdgjs <smhssh9@naver.com>
2022-07-22 12:53:08 +0900

    챕터 9 클래스

---
+++ src/j09_클래스/생성자/CarMain.java
@@ -0,0 +1,16 @@
+package j09_클래스.생성자;
+
+import j09_클래스.Car;
+
+public class CarMain {
+	public static void main(String[] args) {
+		
+			
+		
+		
+		
+
+	}
+	
+
+}
---
+++ src/j09_클래스/생성자/ConstuctorEx2Main.java
@@ -0,0 +1,11 @@
+package j09_클래스.생성자;
+
+public class ConstuctorEx2Main {
+	
+	public static void main(String[] agrs) {
+		ConstuctorEx2 constuctorEx2 = new ConstuctorEx2();
+}
+		
+	
+	
+}
