27f5c413955ba8824911121ca35060c574354b18
thdtkdgjs <smhssh9@naver.com>
2022-08-01 12:54:04 +0900

    챕터 13 최상위클래스

---
+++ src/j13_최상위클래스/Test.java
@@ -0,0 +1,24 @@
+package j13_최상위클래스;
+
+public class Test extends Object {
+
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return super.equals(obj);
+	}
+
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+	
+	public void testMethod() {
+		System.out.println("테스트 메소드");
+	}
+
+}
